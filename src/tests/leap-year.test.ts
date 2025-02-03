69342b71f6638b3b8d95c64520e7b31f33d1083e
ramvignesh-b <ramvignesh@incubyte.co>
2025-02-03 17:31:12 +0530

    test: returns true if year is divisible by 4

--- src/tests/leap-year.test.ts
+++ src/tests/leap-year.test.ts
@@ -5,5 +5,10 @@ describe("Leap Year", () => {
     it("should return false if not divisible by 4", () => {
         const year = new Year(1997);
         expect(year.isLeapYear()).toBe(false);
+    });
+
+    it("should return true if divisible by 4", () => {
+        const year = new Year(1996);
+        expect(year.isLeapYear()).toBe(true);
     })
 })
